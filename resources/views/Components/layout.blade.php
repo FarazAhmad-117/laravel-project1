3b0a1c18ea63d1a08f124c25ce1dc1686df705ea
FarazAhmad-117 <farazahmad31048@gmail.com>
2025-02-07 07:33:06 +0500

    links beng dynamic

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
@@ -9,15 +9,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <!--
-  This example requires updating your template:
+<body class="h-full bg-gray-100">
 
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -30,15 +23,9 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/"
-                                    class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                    aria-current="page">Home</a>
-                                <a href="/about"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-
+                                <x-nav-link link="/" :active="request()->is('/')" label="Home" />
+                                <x-nav-link link="/about" :active="request()->is('about')" label="About" />
+                                <x-nav-link link="/contact" :active="request()->is('contact')" label="Contact" />
                             </div>
                         </div>
                     </div>
@@ -99,12 +86,10 @@
             <div class="md:hidden" id="mobile-menu">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                        aria-current="page">Home</a>
-                    <a href="/about"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                    <a href="/contact"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                    <x-nav-link link="/" active="{{ request()->is('/') }}" label="Home" :mobile="true" />
+                    <x-nav-link link="/about" active="{{ request()->is('about') }}" label="About" :mobile="true" />
+                    <x-nav-link link="/contact" active="{{ request()->is('contact') }}" label="Contact"
+                        :mobile="true" />
                 </div>
                 <div class="border-t border-gray-700 pt-4 pb-3">
                     <div class="flex items-center px-5">
--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,3 +1,22 @@
-<a href="{{$link}}" class="text-sm/6 font-semibold text-gray-900" >
-    {{$label}}
-</a>
+@props([
+    'active' => false,
+    'link' => '#',
+    'label' => 'Link',
+    'mobile' => false,
+    'type' => 'a',
+    'onClick' => '',
+])
+
+@if ($type === 'a')
+    <a href="{{ $link }}"
+        class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium {{ $mobile ? 'block' : '' }} "
+        {{ $attributes }} aria-current="{{ $active ? 'page' : 'false' }}">
+        {{ $label }}
+    </a>
+@else
+    <button onclick="{{ $onClick }}"
+        class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium {{ $mobile ? 'block' : '' }} "
+        {{ $attributes }} aria-current="{{ $active ? 'page' : 'false' }}">
+        {{ $label }}
+    </button>
+@endif
